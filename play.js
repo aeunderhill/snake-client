5d87dd6196945801a9f34ff01cb04f6d448a9f04
Adam Underhill <adameunderhill@gmail.com>
2021-06-02 14:12:50 -0700

    connection function to the server with IP and port

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('incomingData', (data) => {
+    console.log('This is your neighbourhood friendly server', data)
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
