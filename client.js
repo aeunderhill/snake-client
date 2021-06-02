4c0f8f4742603b722d6764d5e2a854be86c9c807
Adam Underhill <adameunderhill@gmail.com>
2021-06-02 14:53:14 -0700

    adding individual moves to the server

--- client.js
+++ client.js
@@ -2,6 +2,14 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
+
+  const name = 'Name: AEU';
+  const up = 'Move: up';
+  const down = 'Move: down';
+  const left = 'Move: left';
+  const right = 'Move: right';
+  
+  
   const conn = net.createConnection({
     host: "135.23.222.131",
     port: 50542
@@ -11,7 +19,12 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    conn.write('Name: AEU'); 
+    conn.write('Name: AEU');
+    setInterval(() => { conn.write(`${up}`)}, 100);
+    setInterval(() => { conn.write(`${down}`)}, 100);
+    setInterval(() => { conn.write(`${left}`)}, 100);
+    setInterval(() => { conn.write(`${right}`)}, 100);
+    
   })
 
   return conn;
